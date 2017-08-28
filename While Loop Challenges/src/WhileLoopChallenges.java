8bc06eb56fdc451a64d655dc9b56866763d481b2
jackson.paul <jackson.paul@MU-C1-12.mullen.pvt.k12.co.us>
2017-08-28 14:11:36 -0600

    initial commit

---
+++ While Loop Challenges/src/WhileLoopChallenges.java
@@ -0,0 +1,32 @@
+
+public class WhileLoopChallenges
+	{
+
+		public static void main(String[] args)
+			{
+			
+			challenge1();
+			challenge2();
+			challenge3();
+
+			}
+
+		private static void challenge1()
+			{
+			
+				
+			}
+
+		private static void challenge2()
+			{
+			
+				
+			}
+
+		private static void challenge3()
+			{
+			
+				
+			}
+
+	}
